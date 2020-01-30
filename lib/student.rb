88eca846341dc57396472ed9044f8997fb898aad
Daniel Reina <dannyflatiron@gmail.com>
2020-01-30 02:38:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,10 +50,10 @@ end
 
   def self.new_from_db(row)
     # binding.pry
-    students = Student.new(row)
-    students.id = row[0]
-    students.name = row[1]
-    students.grade = row[2]
+    student = Student.new(row)
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
     student
   end
 
