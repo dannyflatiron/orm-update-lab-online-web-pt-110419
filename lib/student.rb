cb0b00937442339334976c6195c63db616c345dd
Daniel Reina <dannyflatiron@gmail.com>
2020-01-30 02:51:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,12 +49,15 @@ end
   end
 
   def self.new_from_db(row)
+    # creates a new object from the data provided from the data from the table
     # binding.pry
     id = row[0]
     name = row[1]
     grade = row[2]
     self.new(name, grade, id)
+  end
 
+  def self.find_by_name
   end
 
 end
